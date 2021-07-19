0be095db16be54a6c70a9ae53fb7ad4241b206e9
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-19 08:31:44 -0700

    Dashboard Admin

--- controller/admin/index.js
+++ controller/admin/index.js
@@ -1,5 +1,19 @@
-exports.dahsboardView = (req, res, next) => {
+const Customer = require('../../model/Customer');
+const Item = require('../../model/Item');
+const Bank = require('../../model/Bank');
+const Booking = require('../../model/Booking');
+
+exports.dahsboardView = async (req, res) => {
+    const customer = await Customer.find();
+    const item = await Item.find();
+    const bank = await Bank.find();
+    const booking = await Booking.find();
+
     res.render('admin/dashboard', {
-        title: 'Dashboard Page'
+        title: 'Dashboard Page',
+        customer: customer,
+        item: item,
+        bank: bank,
+        booking: booking,
     });
 }
